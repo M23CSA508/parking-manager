4094a8c52ae2ed1c257177f4209567af0a987df0
m23csa508 <m23csa508@iitj.ac.in>
2024-09-30 14:42:35 +0530

    Optimized imports

--- src/main/java/org/mtech/csa/parking/controller/VehicleController.java
+++ src/main/java/org/mtech/csa/parking/controller/VehicleController.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import org.mtech.csa.parking.entity.Vehicle;
 import org.mtech.csa.parking.service.VehicleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
